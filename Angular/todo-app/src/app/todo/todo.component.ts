de9d2480815f8649fea3678fca147ccb1a982836
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-01-03 23:03:34 +0300

    Uygulama Tasarımı

---
+++ Angular/todo-app/src/app/todo/todo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-app',
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css',
+})
+export class TodoComponent {}
