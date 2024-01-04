6cad16336910efa4a60bf07debe6b6348b285b6b
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-01-04 20:59:32 +0300

    Listeler

--- Angular/todo-app/src/app/todo/todo.component.ts
+++ Angular/todo-app/src/app/todo/todo.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
 })
-export class TodoComponent {}
+export class TodoComponent {
+  private name = 'Furkan';
+
+  // items = ['item 1', 'item 2', 'item 3'];
+
+  items = [
+    { id: 1, description: 'Kahvaltı', action: 'yes' },
+    { id: 2, description: 'Spor', action: 'yes' },
+    { id: 3, description: 'Alışveriş', action: 'no' },
+  ];
+
+  getName() {
+    return this.name;
+  }
+}
