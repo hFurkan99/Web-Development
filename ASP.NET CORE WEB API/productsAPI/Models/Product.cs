10995c9194ef03e985d11002c0a668a1dd914def
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-01-26 22:50:46 +0300

    CoreLayer

--- ASP.NET CORE WEB API/productsAPI/Models/Product.cs
+++ ASP.NET CORE WEB API/productsAPI/Models/Product.cs
@@ -3,7 +3,7 @@ namespace productsAPI.Models
     public class Product
     {
         public int ProductID { get; set; }
-        public string? ProductName { get; set; }
+        public string ProductName { get; set; }
         public decimal Price { get; set; }
         public bool IsActive { get; set; }
     }
