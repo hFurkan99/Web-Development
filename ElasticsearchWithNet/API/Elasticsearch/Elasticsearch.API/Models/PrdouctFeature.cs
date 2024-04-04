0041628b2a46008772dff73faf9fe34cb361bc62
Hasan Furkan Baltacı <furkan.baltaci@basarsoft.com.tr>
2024-04-04 16:46:13 +0300

    ElasticClient ekleme ve Product - ProductFeature sınıflarının oluşturulması.

---
+++ ElasticsearchWithNet/API/Elasticsearch/Elasticsearch.API/Models/PrdouctFeature.cs
@@ -0,0 +1,12 @@
+﻿using Elasticsearch.API.Enums;
+
+namespace Elasticsearch.API.Models
+{
+    public class PrdouctFeature
+    {
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public EColor Color { get; set; }
+
+    }
+}
