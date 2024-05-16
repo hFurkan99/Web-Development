8ab39f2982c048b185eb2d729b2253ad006644c5
Hasan Furkan Baltacı <furkan.baltaci@basarsoft.com.tr>
2024-05-16 16:57:08 +0300

    Tailwind crash course

--- React Router With Data Loading/FastReactPizza/src/features/user/Username.jsx
+++ React Router With Data Loading/FastReactPizza/src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="hidden text-sm font-semibold">Furkan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Furkan</div>;
 }
--- React Router With Data Loading/FastReactPizza/src/ui/AppLayout.jsx
+++ React Router With Data Loading/FastReactPizza/src/ui/AppLayout.jsx
@@ -6,19 +6,19 @@ import Loader from "./Loader";
 export default function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+
   return (
-    <div className="layout">
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div>
-          <Header />
-          <main>
+    <div>
+      <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
+        {isLoading && <Loader />}
+        <Header />
+        <div className="overflow-auto">
+          <main className="mx-auto max-w-3xl overflow-auto p-4 pb-5">
             <Outlet />
           </main>
-          <CartOverview />
         </div>
-      )}
+        <CartOverview />
+      </div>
     </div>
   );
 }
